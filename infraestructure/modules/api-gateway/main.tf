9372f800a6227f8a9dc65816f585436156297a77
chrispam123 <uopechris@gmail.com>
2026-03-23 07:57:36 +0100

    fix(lambda): Add CORS headers in lowercase for HTTP API compatibility

--- infraestructure/modules/api-gateway/main.tf
+++ infraestructure/modules/api-gateway/main.tf
@@ -30,8 +30,8 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_uri        = var.lambda_invoke_arn
   integration_method     = "POST"
   payload_format_version = "2.0"
-}
 
+}
 # Route for GET /
 resource "aws_apigatewayv2_route" "get_random_song" {
   api_id    = aws_apigatewayv2_api.http_api.id
